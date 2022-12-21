480bfe0aabb2f9f00409c4eb4b077b95ecd7cabd
Akanksha <23utopian.97@gmail.com>
2022-12-21 17:57:40 +0530

    initial

--- sweetmeet/models/user.js
+++ sweetmeet/models/user.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
-  age: { type: String, required: true },
+  age: { type: Number, required: true },
   hobbies: { type: Array, required: true },
   languages: { type: Array, required: true },
+  food:{type: Array, required:true},
   password: { type: String, required: true },
 });
 
--- sweetmeet/pages/index.js
+++ sweetmeet/pages/index.js
@@ -1,7 +1,66 @@
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
+import Link from 'next/link';
+
+
 export default function Home() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox>Remember me</Checkbox>
+                {/* <Navlink color={'blue.400'}>Forgot password?</Navlink> */}
+              </Stack>
+              <Button
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                Sign in
+              </Button>
+              <Link href="/signup">Create An account</Link>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
   );
 }
