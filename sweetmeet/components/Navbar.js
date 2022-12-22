2ae090614d8d9aab5f1b64a078b5d90a95e4331a
Akanksha <23utopian.97@gmail.com>
2022-12-22 09:02:14 +0530

    landingpage_changes

---
+++ sweetmeet/components/Navbar.js
@@ -0,0 +1,10 @@
+import { Box , Image} from '@chakra-ui/react'
+import React from 'react'
+
+export default function Navbar(){
+  return (
+    <Box textAlign={'left'} p={'4'} bg={'black'} >
+           <Image src="https://i.ibb.co/Q8146sN/logo.png" width="100px" />
+    </Box>
+  )
+}
--- sweetmeet/pages/index.js
+++ sweetmeet/pages/index.js
@@ -9,6 +9,8 @@ import {
   Button,
   Heading,
   Text,
+  Image,
+  Divider,
   useColorModeValue,
 } from "@chakra-ui/react";
 
@@ -17,6 +19,7 @@ import axios from "axios";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Navbar from "../components/Navbar";
 import { postLogin } from "../redux/user/user.action";
 
 const initialState = { email: "", password: "" };
@@ -41,65 +44,50 @@ export default function Home() {
   }, [isAuth]);
 
   return (
-    <Flex
-      minH={"100vh"}
-      align={"center"}
-      justify={"center"}
-      bg={useColorModeValue("gray.50", "gray.800")}
-    >
-      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-        <Stack align={"center"}>
-          <Heading fontSize={"4xl"}>Sign in to your account</Heading>
-        </Stack>
-        <Box
-          rounded={"lg"}
-          bg={useColorModeValue("white", "gray.700")}
-          boxShadow={"lg"}
-          p={8}
-        >
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input
-                name="email"
-                value={form.email}
-                onChange={handleChange}
-                type="email"
-              />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input
-                name="password"
-                value={form.password}
-                onChange={handleChange}
-                type="password"
-              />
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: "column", sm: "row" }}
-                align={"start"}
-                justify={"space-between"}
-              >
-                <Checkbox>Remember me</Checkbox>
-                {/* <Navlink color={'blue.400'}>Forgot password?</Navlink> */}
+    <Box>
+      <Navbar/>
+       
+       <Flex  minH={"88vh"} align={"center"} justify={"center"} bg={'black'} gap="2rem">
+         <Box>
+             <Image src="https://i.ibb.co/Br558sN/landing.webp" alt="logo" />
+         </Box>
+
+         <Flex align={"center"} justify={"center"}>
+            <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+               <Stack align={"center"}>
+                  <Heading fontSize={"4xl"}>Login in to your account</Heading>
               </Stack>
-              <Button
-                bg={"blue.400"}
-                color={"white"}
-                _hover={{
-                  bg: "blue.500",
-                }}
-                onClick={handleSubmit}
-              >
-                Sign in
-              </Button>
-              <Link href="/signup">Create An account</Link>
+              <Box boxShadow={"lg"} p={8} border={'1px solid pink'} >
+                   <Box marginBottom={'4rem'}>
+                      <Image src="https://i.ibb.co/LS2Q2fD/efc2b0c145dbabbfd1d930a6f80e00eb-removebg-preview.png"/>
+                   </Box>
+                  <Stack spacing={4}>
+                      <FormControl id="email">
+                           <Input name="email" placeholder="email" value={form.email}
+                                 onChange={handleChange} type="email"/>
+                     </FormControl>
+                     <FormControl id="password">
+                           <Input name="password" placeholder="password" value={form.password}
+                                 onChange={handleChange} type="password"/>
+                     </FormControl>
+                     <Stack spacing={10}>
+                        <Button bgGradient='linear(to-r, red.400, orange.500, yellow.500, green.400 )'
+                            color={"white"} _hover={{bgGradient:'linear(to-r, red.400, orange.500)' }}
+                            onClick={handleSubmit}>
+                                 Sign in
+                        </Button>
+                        <Divider/>
+                        <Button bgGradient='linear(to-r, red.400, orange.500, yellow.500, green.400 )'
+                            color={"white"} _hover={{bgGradient:'linear(to-r, red.400, orange.500)' }}
+                            onClick={handleSubmit}>
+                                  <Link href="/signup" align={'center'}> Create an account</Link>
+                            </Button>
+                     </Stack>
+                  </Stack>
+              </Box>
             </Stack>
-          </Stack>
-        </Box>
-      </Stack>
-    </Flex>
+         </Flex>
+       </Flex>
+    </Box>
   );
 }
